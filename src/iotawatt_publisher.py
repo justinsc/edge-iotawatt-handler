900da9a4a9f0fe1f8ddf99a8dca0b2bacb52df9f
Massimo Gaggero <massimo.gaggero@crs4.it>
2018-11-23 18:29:02 +0100

    - moves command line and configuration file parsing from main to a dedicated function; - improves command line and configration testing; - removes deprecated "mqtt_host" and "mqtt_port" GENERAL section options in favour of "mqtt_local_host" and "mqtt_local_port"; - code cleanup to comply to PEP-8.

--- src/iotawatt_publisher.py
+++ src/iotawatt_publisher.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 #
-#  Copyright 2018, CRS4 - Center for Advanced Studies, Research and Development in Sardinia
+#  Copyright 2018, CRS4 - Center for Advanced Studies, Research and Development
+# in Sardinia
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -27,13 +28,13 @@ import datetime
 import configparser
 import paho.mqtt.publish as publish
 from werkzeug.utils import cached_property
-from werkzeug.wrappers import Request
 
 
-MQTT_HOST     = "localhost"     # MQTT Broker address
-MQTT_PORT     = 1883            # MQTT Broker port
+MQTT_LOCAL_HOST = "localhost"     # MQTT Broker address
+MQTT_LOCAL_PORT = 1883            # MQTT Broker port
 INFLUXDB_HOST = "localhost"     # INFLUXDB address
 INFLUXDB_PORT = 8086            # INFLUXDB port
+GPS_LOCATION = "0.0,0.0"        # DEFAULT location
 
 
 APPLICATION_NAME = 'IOTAWATT_publisher'
@@ -47,7 +48,8 @@ class INFLUXDBRequest(flask.Request):
 
     @cached_property
     def get_payload(self):
-        if self.headers.get('content-type') == 'application/x-www-form-urlencoded':
+        _form_content_type = 'application/x-www-form-urlencoded'
+        if self.headers.get('content-type') == _form_content_type:
             l_points = []
             v_payload = self.get_data()
             v_points = v_payload.splitlines()
@@ -55,25 +57,24 @@ class INFLUXDBRequest(flask.Request):
                 l_points.append(
                     dict(
                         zip(
-                            ['tag_set', 'field_set', 'timestamp'], 
-                            _point.decode().split()
-                            )
-                        )
-                    )
+                            ['tag_set', 'field_set', 'timestamp'],
+                            _point.decode().split())))
 
-            return (l_points)
+            return l_points
+
 
 PARAMETERS_MAP = {
-        'A':   'current',
-        'AC':  'apparentPower',
-        'PF':  'powerFactor',
-        'W':   'realPower',
-        'Wh':  'consumedEnergy',
-        'F':   'frequency',
-        'V':   'voltage'
+    'A': 'current',
+    'AC': 'apparentPower',
+    'PF': 'powerFactor',
+    'W': 'realPower',
+    'Wh': 'consumedEnergy',
+    'F': 'frequency',
+    'V': 'voltage'
 }
 
 MESSAGE_PARAMETERS = PARAMETERS_MAP.keys()
+
 
 @app.route("/query", methods=['POST'])
 def query_data():
@@ -86,24 +87,26 @@ def query_data():
     _data = '&'.join(['='.join(_i) for _i in _payload.items()])
 
     _client = influxdb.InfluxDBClient(
-            host=v_influxdb_host,
-            port=v_influxdb_port,
-            username='root', 
-            password='root', 
-            database=_db
-            )
+        host=v_influxdb_host,
+        port=v_influxdb_port,
+        username='root',
+        password='root',
+        database=_db
+    )
 
     _dbs = _client.get_list_database()
     if _db not in [_d['name'] for _d in _dbs]:
-        v_logger.info("InfluxDB database '{:s}' not found. Creating a new one.".format(_db))
+        v_logger.info(
+            "InfluxDB database '{:s}' not found. Creating a new one.".
+            format(_db))
         _client.create_database(_db)
 
     try:
         _result = _client.request(
-                'query', 
-                'POST', 
-                params=_data, 
-                expected_response_code=200)
+            'query',
+            'POST',
+            params=_data,
+            expected_response_code=200)
     except Exception as ex:
         v_logger.error(ex)
     finally:
@@ -113,12 +116,14 @@ def query_data():
 
     return _response
 
+
 @app.route("/write", methods=['POST'])
 def publish_data():
     v_logger = app.config['LOGGER']
-    
-    v_mqtt_host  = app.config['MQTT_HOST']
-    v_mqtt_port  = app.config['MQTT_PORT']
+
+    v_mqtt_local_host = app.config['MQTT_LOCAL_HOST']
+    v_mqtt_local_port = app.config['MQTT_LOCAL_PORT']
+    v_topic = app.config['MQTT_TOPIC']
     v_influxdb_host = app.config['INFLUXDB_HOST']
     v_influxdb_port = app.config['INFLUXDB_PORT']
 
@@ -127,25 +132,27 @@ def publish_data():
     _db = _args.get('db')
 
     _client = influxdb.InfluxDBClient(
-            host=v_influxdb_host,
-            port=v_influxdb_port,
-            username='root', 
-            password='root', 
-            database=_db
-            )
+        host=v_influxdb_host,
+        port=v_influxdb_port,
+        username='root',
+        password='root',
+        database=_db
+    )
 
     _dbs = _client.get_list_database()
     if _db not in [_d['name'] for _d in _dbs]:
-        v_logger.info("InfluxDB database '{:s}' not found. Creating a new one.".format(_db))
+        v_logger.info(
+            "InfluxDB database '{:s}' not found. Creating a new one.".
+            format(_db))
         _client.create_database(_db)
 
     try:
         _result = _client.request(
-                'write', 
-                'POST', 
-                params=_args, 
-                data=_data, 
-                expected_response_code=204)
+            'write',
+            'POST',
+            params=_args,
+            data=_data,
+            expected_response_code=204)
         v_logger.debug("Insert data into InfluxDB: {:s}".format(str(_data)))
     except Exception as ex:
         v_logger.error(ex)
@@ -157,19 +164,17 @@ def publish_data():
     v_messages = []
 
     try:
-        v_topic = app.config['MQTT_TOPIC']
-
         v_payload = flask.request.get_payload
 
         # Creates a dictionary with the sensor data
         for v_measure in v_payload:
             _sensor_tree = dict()
 
-            _tag       = v_measure['tag_set']
-            _, _value  = v_measure['field_set'].split('=')
+            _tag = v_measure['tag_set']
+            _, _value = v_measure['field_set'].split('=')
             _timestamp = v_measure['timestamp']
-            _dateObserved = datetime.datetime.fromtimestamp(int(_timestamp),
-                        tz=datetime.timezone.utc).isoformat()
+            _dateObserved = datetime.datetime.fromtimestamp(
+                int(_timestamp), tz=datetime.timezone.utc).isoformat()
 
             _label, _, _parameter = _tag.partition('_')
             _station_id = 'IOTAWATT'
@@ -182,21 +187,26 @@ def publish_data():
                     PARAMETERS_MAP[_parameter]: _value,
                     'timestamp': _timestamp,
                     'dateObserved': _dateObserved,
-                    })
+                })
 
             # Insofar, one message is sent for each sensor
             for _label, _data in _sensor_tree.items():
                 _message = dict()
                 _message["payload"] = json.dumps(_data)
-                _message["topic"] = "EnergyMonitor/{}.{}".format(_station_id, _label)
+                _message["topic"] = "EnergyMonitor/{}.{}".format(
+                    _station_id, _label)
                 _message['qos'] = 0
                 _message['retain'] = False
-    
+
                 v_messages.append(_message)
 
-        v_logger.debug("MQTT message, broker:\'{:s}:{:d}\', "
-            "messages:\'{:s}\'".format(v_mqtt_host, v_mqtt_port, json.dumps(v_messages)))
-        publish.multiple(v_messages, hostname=v_mqtt_host, port=v_mqtt_port)
+        v_logger.debug(
+            "Message topic:\'{:s}\', broker:\'{:s}:{:d}\', "
+            "message:\'{:s}\'".format(
+                v_topic, v_mqtt_local_host, v_mqtt_local_port,
+                json.dumps(v_messages)))
+        publish.multiple(v_messages, hostname=v_mqtt_local_host,
+                         port=v_mqtt_local_port)
     except socket.error:
         pass
 
@@ -207,91 +217,134 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
+def configuration_parser(p_args=None):
+    pre_parser = argparse.ArgumentParser(add_help=False)
+
+    pre_parser.add_argument(
+        '-c', '--config-file', dest='config_file', action='store',
+        type=str, metavar='FILE',
+        help='specify the config file')
+
+    args, remaining_args = pre_parser.parse_known_args(p_args)
+
+    v_general_config_defaults = {
+        'mqtt_local_host'     : MQTT_LOCAL_HOST,
+        'mqtt_local_port'     : MQTT_LOCAL_PORT,
+        'logging_level' : logging.INFO,
+        'influxdb_host' : INFLUXDB_HOST,
+        'influxdb_port' : INFLUXDB_PORT,
+        'gps_location'  : GPS_LOCATION,
+    }
+
+    v_specific_config_defaults = {
+    }
+
+    v_config_section_defaults = {
+        'GENERAL': v_general_config_defaults,
+        APPLICATION_NAME: v_specific_config_defaults
+    }
+
+    # Default config values initialization
+    v_config_defaults = {}
+    v_config_defaults.update(v_general_config_defaults)
+    v_config_defaults.update(v_specific_config_defaults)
+
+    if args.config_file:
+        _config = configparser.ConfigParser()
+        _config.read_dict(v_config_section_defaults)
+        _config.read(args.config_file)
+
+        # Filter out GENERAL options not listed in v_general_config_defaults
+        _general_defaults = {_key: _config.get('GENERAL', _key) for _key in
+                             _config.options('GENERAL') if _key in
+                             v_general_config_defaults}
+
+        # Updates the defaults dictionary with general and application specific
+        # options
+        v_config_defaults.update(_general_defaults)
+        v_config_defaults.update(_config.items(APPLICATION_NAME))
+
+    parser = argparse.ArgumentParser(
+        parents=[pre_parser],
+        description=('Collects data from Luftdaten Fine Dust sensor and '
+                     'publish them to a local MQTT broker.'),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.set_defaults(**v_config_defaults)
+
+    parser.add_argument(
+        '-l', '--logging-level', dest='logging_level', action='store',
+        type=int,
+        help='threshold level for log messages (default: {})'.
+        format(logging.INFO))
+    parser.add_argument(
+        '--mqtt-host', dest='mqtt_local_host', action='store',
+        type=str,
+        help='hostname or address of the local broker (default: {})'.
+        format(MQTT_LOCAL_HOST))
+    parser.add_argument(
+        '--mqtt-port', dest='mqtt_local_port', action='store',
+        type=int,
+        help='port of the local broker (default: {})'.
+        format(MQTT_LOCAL_PORT))
+    parser.add_argument(
+        '--influxdb-host', dest='influxdb_host', action='store',
+        type=str,
+        help='hostname or address of the influx database (default: {})'.
+        format(INFLUXDB_HOST))
+    parser.add_argument(
+        '--influxdb-port', dest='influxdb_port', action='store',
+        type=int,
+        help='port of the influx database (default: {})'.format(INFLUXDB_PORT))
+    parser.add_argument(
+        '--gps-location', dest='gps_location', action='store',
+        type=str,
+        help=('GPS coordinates of the sensor as latitude,longitude '
+              '(default: {})').format(GPS_LOCATION))
+
+    args = parser.parse_args(remaining_args)
+    return args
+
+
 def main():
     # Initializes the default logger
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO)
     logger = logging.getLogger(APPLICATION_NAME)
 
     # Checks the Python Interpeter version
     if (sys.version_info < (3, 0)):
-        ###TODO: Print error message here
+        logger.fatal("This software requires Python version >= 3.0: exiting.")
         sys.exit(-1)
 
-    pre_parser = argparse.ArgumentParser(add_help=False)
-
-    pre_parser.add_argument('-c', '--config-file', dest='config_file', action='store',
-        type=str, metavar='FILE',
-        help='specify the config file')
-
-    args, remaining_args = pre_parser.parse_known_args()
-
-    v_config_defaults = {
-        'mqtt_host'     : MQTT_HOST,
-        'mqtt_port'     : MQTT_PORT,
-        'logging_level' : logging.INFO,
-        'influxdb_host' : INFLUXDB_HOST,
-        'influxdb_port' : INFLUXDB_PORT,
-        }
-
-    v_config_section_defaults = {
-        APPLICATION_NAME: v_config_defaults
-        }
-
-    if args.config_file:
-        v_config = configparser.ConfigParser()
-        v_config.read_dict(v_config_section_defaults)
-        v_config.read(args.config_file)
-
-        v_config_defaults = dict(v_config.items(APPLICATION_NAME))
-
-    parser = argparse.ArgumentParser(parents=[pre_parser], 
-            description='Collects data from Luftdaten Fine Dust sensor and publish them to a local MQTT broker.',
-            formatter_class=argparse.RawDescriptionHelpFormatter)
-
-    parser.set_defaults(**v_config_defaults)
-
-    parser.add_argument('-l', '--logging-level', dest='logging_level', action='store', 
-        type=int,
-        help='threshold level for log messages (default: {})'.format(logging.INFO))
-    parser.add_argument('--mqtt-host', dest='mqtt_host', action='store', 
-        type=str,
-        help='hostname or address of the local broker (default: {})'
-            .format(MQTT_HOST))
-    parser.add_argument('--mqtt-port', dest='mqtt_port', action='store', 
-        type=int,
-        help='port of the local broker (default: {})'.format(MQTT_PORT))
-    parser.add_argument('--influxdb-host', dest='influxdb_host', action='store', 
-        type=str,
-        help='hostname or address of the influx database (default: {})'
-            .format(INFLUXDB_HOST))
-    parser.add_argument('--influxdb-port', dest='influxdb_port', action='store', 
-        type=int,
-        help='port of the influx database (default: {})'.format(INFLUXDB_PORT))
-
-    args = parser.parse_args(remaining_args)
+    args = configuration_parser()
 
     logger.setLevel(args.logging_level)
+
     logger.info("Starting {:s}".format(APPLICATION_NAME))
     logger.debug(vars(args))
 
     signal.signal(signal.SIGINT, signal_handler)
 
     v_mqtt_topic = 'sensor/' + 'IOTAWATT'
+    v_latitude, v_longitude = map(float, args.gps_location.split(','))
 
     config_dict = {
-            'LOGGER'     : logger,
-            'MQTT_HOST'  : args.mqtt_host,
-            'MQTT_PORT'  : args.mqtt_port,
-            'LOG_LEVEL'  : args.logging_level,
-            'MQTT_TOPIC' : v_mqtt_topic,
+        'LOGGER'     : logger,
+        'MQTT_LOCAL_HOST'  : args.mqtt_local_host,
+        'MQTT_LOCAL_PORT'  : args.mqtt_local_port,
+        'LOG_LEVEL'  : args.logging_level,
+        'MQTT_TOPIC' : v_mqtt_topic,
 
-            'INFLUXDB_HOST' : args.influxdb_host,
-            'INFLUXDB_PORT' : args.influxdb_port,
-            }
+        'INFLUXDB_HOST' : args.influxdb_host,
+        'INFLUXDB_PORT' : args.influxdb_port,
+    }
 
     app.config.from_mapping(config_dict)
     app.request_class = INFLUXDBRequest
     app.run(host='0.0.0.0')
+
 
 if __name__ == "__main__":
     main()
